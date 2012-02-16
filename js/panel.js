285798b0d9ede487c410d1612f877433f03d4f3b
sergiorey <sergio@sergiorc.es>
2012-02-16 17:34:45 +0100

    Panel is divided in rows and columns

--- js/panel.js
+++ js/panel.js
@@ -5,11 +5,17 @@ function Panel(name, x, y, width, height) {
 
 	/* PROPERTIES */
 	
-	this.name = name || "empty"
+	this.name = name || "empty";
 	this.x = x || 0;
 	this.y = y || 0;
 	this.width = width || 0;
 	this.height = height || 0;
+	
+	// Panel of 1 row x 1 column as default
+	this.rowsCount = 1;
+	this.columnsCount = 1;
+	
+	// Property to contain other Actors (cards,..)
 	this.container = new CAAT.ActorContainer()
 		.setBounds(this.x, this.y, this.width, this.height);
 		
