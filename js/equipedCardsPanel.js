a60fd1d970c3268c08fe09897d7f47e0e6bbe2a2
sergiorey <sergio@sergiorc.es>
2012-02-17 14:05:38 +0100

    EquipedCardsPanel - WIP

--- js/equipedCardsPanel.js
+++ js/equipedCardsPanel.js
@@ -6,29 +6,38 @@ function EquipedCardsPanel(id) {
 	
 	/* PROPERTIES */
 	
-	this.MAX_CARDS = 20;
+	this.MAX_SLOTS = 5;
+	this.SLOT_PADDING = 5; // Vertical separation between slots
 	
 	// Call constructor with parameters
 	this.base = Panel;
 	this.base(id);
-	
 	this.slots = []; // Slots containing cards
 	
 	/* METHODS */
 	
 	this.initSlots = function() {
 		
-		var slotAir = new SlotElement();
-		slotAir.container.setBounds(
-				this.container.x,
-				this.container.y,
-				178,88
-				)
-			.setFillStyle("#fbff87");
+		var nextSlotY = this.container.y;
 		
-		slotAir.name = "slot air";
-		
-		this.addSlot(slotAir);
+		for(var slotIndex = 0; slotIndex < 5; slotIndex++) {
+			
+			var newSlot = new SlotElement();
+			
+			newSlot.container.setBounds(
+					this.container.x,
+					nextSlotY,
+					178,88
+					)
+				.setFillStyle("#fbff87");
+			
+			newSlot.name = "slot" + slotIndex;
+			
+			this.addSlot(newSlot);	
+			
+			// Calculate next slot Y position
+			nextSlotY += 88 + this.SLOT_PADDING;
+		}
 	}
 	
 	this.addSlot = function(slot) {
