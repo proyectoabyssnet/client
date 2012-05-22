bd25dbe91568796ff1b640e0f060155b6f8a4559
sergiorey <sergio@sergiorc.es>
2012-05-22 12:30:54 +0200

    Added 2 files

---
+++ js/cardDisplayer.js
@@ -0,0 +1,93 @@
+/*
+* Object to display all cards contained in a slotElement
+*/
+
+var CardsDisplayer = {};
+
+Object.defineProperties( CardsDisplayer, {
+	
+	slotId:		{ value: "<empty>", writable: true },
+	container:	{ value: null, writable: true },
+	position:	{ value: [0,0], writable: true },
+	
+	init: {
+		
+		value: function(id, slotId, x, y) {
+													
+			this.slotId = slotId;
+								
+			var buttonSprite = new CAAT.SpriteImage()
+				.initialize(window['director'].getImage('stars'),
+				1,6);
+
+			
+			this.container = new CAAT.Actor().setPosition(x,y);	
+			this.position[0] = this.container.x;
+			this.position[1] = this.container.y;
+			var cont = this.container;	
+			var slotId = this.slotId;	
+			var displayCardsFunction = this.displayCardsForSlot;
+			
+			this.container.setAsButton(
+					buttonSprite.getRef(),
+					0,1,2,0, 
+					function (button) {
+					
+						// Access to cells and display contained cards
+						
+						var equipedCardsPanel = window['equiped_cards_panel'];
+						
+						if (equipedCardsPanel != null) {
+							
+							var startX = equipedCardsPanel.container
+								.findActorById("slot-" + slotId).width;
+								
+							displayCardsFunction(
+								equipedCardsPanel.slotElements[slotId],
+								startX
+							);														
+						}
+					}
+				);
+			
+						
+		}, enumerable: true
+	},
+	
+	displayCardsForSlot: {
+	
+		value: function(slotElement, startX) { 
+			
+			var numberOfCards = 0;
+			var cardContainer = null;
+			var nextCardPositionX = startX; //slotElement[0].cards[0].container.x;
+			var nextCardPositionY = slotElement[0].cards[0].container.y;
+			var cardPadding = 4;
+			
+			// Run through cells
+			for (var cell = 0; cell < 2; cell++) {
+
+				numberOfCards = slotElement[cell].cards.length;
+				
+				// Run through list of cards contained in a cell
+				for (var card = 0; card < numberOfCards; card++) {
+					 
+					 // Display cards horizontally
+					 cardContainer = slotElement[cell].cards[card].container;
+					 
+					 cardContainer.setLocation(
+					 	nextCardPositionX, 
+					 	nextCardPositionY
+					 );
+					 
+					 console.log("Displaying card: " + cardContainer.getId() +
+					 	+ ", x =  " + nextCardPositionX);					
+					 nextCardPositionX += cardContainer.width + cardPadding;
+
+				}				
+				
+			}
+			
+		}, enumerable: true
+	}
+});
---
+++ js/detachedCardsPanel.js
@@ -0,0 +1,66 @@
+/*
+* Detached cards panel
+*/
+
+var DetachedCardsPanel = Object.create( Panel );
+
+Object.defineProperties( DetachedCardsPanel, {
+
+	maxCards:		{ value: 0, writable: true },
+	cellWidth:		{ value: 0, writable: true },
+	cellHeight:		{ value: 0, writable: true },
+	cellPadding:	{ value: 4, writable: true },
+	
+	init: {
+	
+		value: function() {
+		
+			this.initPanel();
+			var slotBackgroundImage = window['director'].getImage("slot-bg");
+			this.cellWidth = slotBackgroundImage.width;
+			this.cellHeight = slotBackgroundImage.height;
+			this.cellPadding = 4;			
+			this.maxCards = 3;
+			
+		}, enumerable: true
+	},
+	
+	initCells: {
+	
+		value: function() {
+		
+			var nextCellXPosition = 1;
+			
+			for(var i=0; i < this.maxCards; i++) {
+		
+				// Create 1 cell to store 1 card
+				cell = Object.create( Slot );
+				cell.init("dc_cell_" + i);
+				cell.setMaxCards(1); // 1 card per cell				
+				cell.container.setSize(this.cellWidth, this.cellHeight)
+					.setLocation(
+						nextCellXPosition + this.cellPadding,  
+						1 + this.cellPadding
+						)
+					.setFillStyle("#ccc")
+					.setAlpha(0.5);
+	
+				// Store cell... 	
+				//this.cells.push( cell );				
+		
+				// Calculate next cell position				
+				nextCellXPosition += cell.container.width +
+					this.cellPadding; 
+		
+				this.container.addChild(cell.container);
+			}
+			
+			// Reset panel width
+			
+			this.container.setSize(this.maxCards * this.cellWidth);
+			
+						
+		}, enumerable: true
+	}
+	
+});
